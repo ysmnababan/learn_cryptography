b1fe7f90f12a6e951c67a4d6dd48763e4a61f2ff
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-09-10 14:33:33 +0700

    feat: enhance README and add key generation script for manual mTLS setup

--- notes/zero_trust_policy/manual-mTLS/README.md
+++ notes/zero_trust_policy/manual-mTLS/README.md
@@ -1,2 +1,64 @@
-# Demonstration of Manual mTLS
+# Manual mTLS Demonstration
 
+This project demonstrates Mutual TLS (mTLS) authentication by manually creating certificates.
+
+## Prerequisites
+
+- [Go](https://golang.org/) installed
+- Bash shell (for running scripts)
+
+## Steps to Run
+
+1. **Generate Certificates**
+
+    Run the following command to create the necessary certificates:
+    ```bash
+    bash key.sh
+    ```
+
+2. **Start the Server**
+
+    Launch the server using:
+    ```bash
+    go run . --mode=server
+    ```
+
+3. **Run the Client**
+
+    In a separate terminal, start the client:
+    ```bash
+    go run . --mode=client
+    ```
+
+## Expected Output
+
+### Server
+
+The server will display details about the incoming request and TLS state:
+```text
+2025/09/10 14:28:29 >>>>>>>>>>>>>>>> Header <<<<<<<<<<<<<<<<
+2025/09/10 14:28:29 Accept-Encoding:gzip
+2025/09/10 14:28:29 User-Agent:Go-http-client/1.1
+2025/09/10 14:28:29 >>>>>>>>>>>>>>>> State <<<<<<<<<<<<<<<<
+2025/09/10 14:28:29 Version: 304
+2025/09/10 14:28:29 HandshakeComplete: true
+2025/09/10 14:28:29 DidResume: false
+2025/09/10 14:28:29 CipherSuite: 1301
+2025/09/10 14:28:29 NegotiatedProtocol:
+2025/09/10 14:28:29 NegotiatedProtocolIsMutual: true
+2025/09/10 14:28:29 Certificate chain:
+2025/09/10 14:28:29  0 s:/C=[SO]/ST=[Earth]/L=[Mountain]/O=[Client-A]/OU=[Client-A-OU]/CN=localhost
+2025/09/10 14:28:29    i:/C=[SO]/ST=[Earth]/L=[Mountain]/O=[MegaEase]/OU=[MegaCloud]/CN=localhost
+```
+
+### Client
+
+The client will display the server's response:
+```text
+2025/09/10 14:28:29 Response body: nice !!!
+```
+
+## Notes
+
+- Ensure both server and client use the generated certificates for successful mTLS authentication.
+- Modify certificate details in `key.sh` as needed for your environment.
--- notes/zero_trust_policy/manual-mTLS/main.go
+++ notes/zero_trust_policy/manual-mTLS/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"flag"
 	"io"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -15,32 +17,83 @@ var (
 	mode = flag.String("mode", "server", "server or client")
 )
 
+func printHeader(r *http.Request) {
+	log.Print(">>>>>>>>>>>>>>>> Header <<<<<<<<<<<<<<<<")
+	// Loop over header names
+	for name, values := range r.Header {
+		// Loop over all values for the name.
+		for _, value := range values {
+			log.Printf("%v:%v", name, value)
+		}
+	}
+}
+
+func printConnState(state *tls.ConnectionState) {
+	log.Print(">>>>>>>>>>>>>>>> State <<<<<<<<<<<<<<<<")
+
+	log.Printf("Version: %x", state.Version)
+	log.Printf("HandshakeComplete: %t", state.HandshakeComplete)
+	log.Printf("DidResume: %t", state.DidResume)
+	log.Printf("CipherSuite: %x", state.CipherSuite)
+	log.Printf("NegotiatedProtocol: %s", state.NegotiatedProtocol)
+
+	log.Print("Certificate chain:")
+	for i, cert := range state.PeerCertificates {
+		subject := cert.Subject
+		issuer := cert.Issuer
+		log.Printf(" %d s:/C=%v/ST=%v/L=%v/O=%v/OU=%v/CN=%s", i, subject.Country, subject.Province, subject.Locality, subject.Organization, subject.OrganizationalUnit, subject.CommonName)
+		log.Printf("   i:/C=%v/ST=%v/L=%v/O=%v/OU=%v/CN=%s", issuer.Country, issuer.Province, issuer.Locality, issuer.Organization, issuer.OrganizationalUnit, issuer.CommonName)
+	}
+}
+
 func runServer(addr string) {
 	e := echo.New()
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
 	e.GET("/hello", func(c echo.Context) error {
+		printHeader(c.Request())
+		printConnState(c.Request().TLS)
 		return c.String(200, "nice !!!")
 	})
-	s := http.Server{
-		Addr:    addr,
-		Handler: e,
+	ca, err := os.ReadFile("./certs/ca.crt")
+	if err != nil {
+		log.Println("error read ca", err)
 	}
-	err := s.ListenAndServeTLS("server.crt", "server.key")
+	certPool := x509.NewCertPool()
+	certPool.AppendCertsFromPEM(ca)
+
+	conf := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+		ClientAuth: tls.RequireAndVerifyClientCert,
+		ClientCAs:  certPool,
+	}
+	s := http.Server{
+		Addr:      addr,
+		Handler:   e,
+		TLSConfig: conf,
+	}
+
+	err = s.ListenAndServeTLS("./certs/server.crt", "./certs/server.key")
 	if err != nil {
 		panic(err)
 	}
 }
 
 func runClient(port string) {
-	cert, err := tls.LoadX509KeyPair("client.crt", "client.key")
+	ca, err := os.ReadFile("./certs/ca.crt")
+	if err != nil {
+		log.Println("error read ca", err)
+	}
+	certPool := x509.NewCertPool()
+	certPool.AppendCertsFromPEM(ca)
+	cert, err := tls.LoadX509KeyPair("./certs/client.a.crt", "./certs/client.a.key")
 	if err != nil {
 		log.Println("error loading", err)
 	}
 	client := &http.Client{
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true,
+				RootCAs:      certPool,
 				Certificates: []tls.Certificate{cert},
 			},
 		},
